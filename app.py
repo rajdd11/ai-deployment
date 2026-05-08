7f71fa77d66f0d18e89f840fb67358fa8cadcbce
Rajesh <rajdd11@gmail.com>
2026-05-08 18:26:54 +0530

    Made Chat interactive and now it looks more like AI assistant

--- app.py
+++ app.py
@@ -8,13 +8,32 @@ client = genai.Client(
     api_key=os.getenv("GOOGLE_API_KEY")
 )
 
-question = input("Ask something: ")
+print("AI Assistant Started")
+print("Type 'exit' to quit\n")
 
-# The model string has been updated to a supported version
-response = client.models.generate_content(
-    model="gemini-2.5-flash", 
-    contents=question
-)
+while True:
+    question = input("You: ")
 
-print("\nAI Response:\n")
-print(response.text)
+    if question.lower() == "exit":
+        break
+
+    try:  
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=f"""
+            You are a professional IT support assistant.
+            and you are man of few words
+    
+            User question:
+            {question}
+            """
+        )
+    
+        print("\nAI:")
+        print(response.text)
+        print("\n")
+
+    except Exception as e:
+        print("\nError:",e)
+        print(e)
+        print("\n")
